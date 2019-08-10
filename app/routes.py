43be3197dee6ad8aef72563b8f7c3aea55a81f26
Mustafa <m.mustafaahmed@hotmail.com>
2019-08-10 16:02:26 +1000

    Main page updated

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,7 @@
 from flask import *
 from app import app
-from app.forms import LoginForm
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    
-    return render_template('login.html', title="Sign in", form=form)
+
+    return render_template('entry_page.html', title="Start reading!", form=form)
