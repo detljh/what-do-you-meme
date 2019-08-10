c93528128a42d9c7fc010e9ab5c4e75761b0294f
Lisa <lisa.ng8@hotmail.com>
2019-08-10 10:08:28 +0000

    added hover

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,6 @@ from app.models import *
 from config import Config
 import os
 
-HAPPY_IMAGE_PATH = Config.STATIC_FOLDER + 'images/happy/'
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -15,9 +14,5 @@ def login():
 
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    h = HappyMeme(image_path=HAPPY_IMAGE_PATH + 'dicaprio3-583e33155f9b58d5b19e3a00.jpg')
-    db.session.add(h)
-    db.session.commit()
-    file_path = HappyMeme.query.get(1).image_path
     filepath = "/static/images/happy/dicaprio3-583e33155f9b58d5b19e3a00.jpg"
-    return render_template('home.html', src=filepath)
+    return render_template('home.html', src=filepath, article="news article")
