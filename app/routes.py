cb99576a71df1c196b54ba7b89141321918e8a7b
Lisa <lisa.ng8@hotmail.com>
2019-08-10 10:10:32 +0000

    removed extra login function

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,5 @@
 from flask import *
 from app import app
-
-@app.route('/', methods=['GET', 'POST'])
-def login():
-
-    return render_template('entry_page.html', title="Start reading!", form=form)
 from app.forms import LoginForm
 from app.models import *
 from config import Config
