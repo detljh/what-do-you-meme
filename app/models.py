2d286c5fc713da58612fb0a665e7a3d0fac73db6
Lisa <lisa.ng8@hotmail.com>
2019-08-10 03:32:53 +0000

    added password checking in User model

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from werkzeug.security import *
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,6 +9,12 @@ class User(db.Model):
     coin = db.Column(db.Integer, default=0)
     memes = db.relationship('MemeCard', backref='author', lazy='dynamic')
     
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
 class MemeCard(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     meme_image = db.Column(db.String(128))
