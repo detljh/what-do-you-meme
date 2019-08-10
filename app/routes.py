0cc170761adb29f7667e0d370b8b21fa266c9782
Mustafa <m.mustafaahmed@hotmail.com>
2019-08-10 22:32:53 +1000

    BS4 cause thats better

--- app/routes.py
+++ app/routes.py
@@ -11,26 +11,12 @@ post_number = 0
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    #form = LoginForm()
     image = "/static/images/intro.jpg"
     return render_template('login.html', title="Meme the news", image_src=image)
-
-# @app.route('/home', methods=['GET', 'POST'])
-# def home():
-#     h = HappyMeme(image_path=HAPPY_IMAGE_PATH + 'dicaprio3-583e33155f9b58d5b19e3a00.jpg')
-#     db.session.add(h)
-#     db.session.commit()
-#     file_path = HappyMeme.query.get(1).image_path
-#     filepath = "/static/images/happy/dicaprio3-583e33155f9b58d5b19e3a00.jpg"
-#     return render_template('home.html', src=filepath)
 
 @app.route('/start', methods=['GET', 'POST'])
 def start():
     filepath = "/static/images/intro.jpg"
-    scraper = NewsScraper("./chromedriver")
-    title, content = scraper.scrape_headline_and_content()
-    title_text = [i.text for i in title]
-    content_text = [i.text for i in content]
-    time.sleep(1)
-    scraper.close()
-    return render_template("start.html", src=filepath, article=[title_text[0], content_text[0]])
+    scraper = NewsScraper()
+    title, content = scraper.scrape_headline_title(), scraper.scrape_content()
+    return render_template("start.html", src=filepath, article=[title[0], content[0]])
--- app/scraper.py
+++ app/scraper.py
@@ -1,60 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
 import time
+import requests
 
 class NewsScraper():
-    def __init__(self, location):
-        self.driver_location = location
-        self.browser = "https://www.dailytelegraph.com.au/news/breaking-news"
+    def __init__(self):
+        url = "https://www.dailytelegraph.com.au/news/breaking-news"
+        r = requests.get(url)
+        data = r.text
+        self.soup = BeautifulSoup(data,"lxml")
     
-    def scrape_headline_and_content(self):
-        option = webdriver.ChromeOptions()
-        option.add_argument(" - incognito")
-        self.driver = webdriver.Chrome(executable_path=self.driver_location, chrome_options=option)
-        self.driver.get(self.browser)
-        time.sleep(3)
-        news_title = self.driver.find_elements(By.CLASS_NAME, "tge-cardv2_title")
-        news_content = self.driver.find_elements(By.CLASS_NAME, "tge-cardv2_standfirst")
-        return (news_title, news_content) 
+    def scrape_headline_title(self):
+        title_text = self.soup.find_all("h2", {"class": "tge-cardv2_title"})
+        return [i.text for i in title_text]
 
-    def close(self):
-        time.sleep(1)
-        self.driver.close()
-
-# scraper = NewsScraper()
-# title, content = scraper.scrape_headline_and_content()
-# for i in zip(title, content):
-#     print(i[0].text, i[1].text)
-
-# scraper.close()
-
-# option = webdriver.ChromeOptions()
-# option.add_argument(" - incognito")
-
-# location = input("Enter path for chrometools")
-
-# if len(location) == 0:
-#     location="venv/chromedriver"
-
-# browser = webdriver.Chrome(executable_path=location, chrome_options=option)
-# browser.get("https://www.dailytelegraph.com.au/news/breaking-news")
-
-# time.sleep(2)
-
-# news_title = browser.find_elements(By.CLASS_NAME, "tge-cardv2_title")
-# news_content = browser.find_elements(By.CLASS_NAME, "tge-cardv2_standfirst")
-
-# print(len(news_title))
-# for i,j in zip(news_title, news_content):
-#     print(i.text)
-#     print(j.text)
-#     print("\n NEXT---> \n")
-# try:
-#     browser.quit()
-#     browser.close()
-# except:
-#     exit(0)
+    def scrape_content(self):
+        content_text = self.soup.find_all("p", {"class": "tge-cardv2_standfirst"})
+        return [i.text for i in content_text]
