3569b5ac10b049bb5da2d2f05a40c61265f84246
Mustafa <m.mustafaahmed@hotmail.com>
2019-08-10 19:56:59 +1000

    image_storing

--- app/routes.py
+++ app/routes.py
@@ -9,15 +9,19 @@ HAPPY_IMAGE_PATH = Config.STATIC_FOLDER + 'images/happy/'
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    #form = LoginForm()
+    image = "/static/images/intro.jpg"
+    return render_template('home.html', title="Meme the news", image_src=image)
 
-    return render_template('login.html', title="Sign in", form=form)
+# @app.route('/home', methods=['GET', 'POST'])
+# def home():
+#     h = HappyMeme(image_path=HAPPY_IMAGE_PATH + 'dicaprio3-583e33155f9b58d5b19e3a00.jpg')
+#     db.session.add(h)
+#     db.session.commit()
+#     file_path = HappyMeme.query.get(1).image_path
+#     filepath = "/static/images/happy/dicaprio3-583e33155f9b58d5b19e3a00.jpg"
+#     return render_template('home.html', src=filepath)
 
-@app.route('/home', methods=['GET', 'POST'])
-def home():
-    h = HappyMeme(image_path=HAPPY_IMAGE_PATH + 'dicaprio3-583e33155f9b58d5b19e3a00.jpg')
-    db.session.add(h)
-    db.session.commit()
-    file_path = HappyMeme.query.get(1).image_path
-    filepath = "/static/images/happy/dicaprio3-583e33155f9b58d5b19e3a00.jpg"
-    return render_template('home.html', src=filepath)
+@app.route('/start', methods=['GET', 'POST'])
+def start():
+    return render_template("start.html")
