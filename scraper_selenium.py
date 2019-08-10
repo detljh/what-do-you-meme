da3755937b5ec76d01407d815a35644cb1bf48a8
Mustafa <m.mustafaahmed@hotmail.com>
2019-08-10 16:49:13 +1000

    Scraper working

--- scraper_selenium.py
+++ scraper_selenium.py
@@ -3,7 +3,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
+import time
 
 option = webdriver.ChromeOptions()
 option.add_argument(" - incognito")
@@ -14,11 +15,23 @@ if len(location) == 0:
     location="/Users/mospy26/Documents/what-do-you-meme/chromedriver"
 
 browser = webdriver.Chrome(executable_path=location, chrome_options=option)
+browser.get("https://www.reuters.com/news/archive/worldNews?date=today")
 
+time.sleep(2)
+
+news_photo = browser.find_elements(By.CLASS_NAME, "story-photo")
+news_title = browser.find_elements(By.CLASS_NAME, "story-title")
+news_content = browser.find_elements(By.TAG_NAME, "p")
+
+
+for i,j in zip(news_title, news_content):
+    print(i.text)
+    print("\n")
+    print(j.text)
+    print("\n NEXT---> \n")
 try:
-    element = WebDriverWait(browser, 100).until(
-        EC.presence_of_element_located((By.ID, "select-text"))
-    )
-    print("Yes")
-finally:
     browser.quit()
+    browser.close()
+except:
+    exit(0)
+os.system("taskkill chrome.exe")
